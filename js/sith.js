31c31730cafe84a3a800a8618e778f0f7c763898
Mery De Freitas <67561739+MeryDeFreitas@users.noreply.github.com>
2022-07-30 11:44:48 -0300

    Add files via upload

---
+++ js/sith.js
@@ -0,0 +1,5 @@
+alert("Si no estás conmigo ¡Eres mi enemigo!")
+
+//Se puede hacer un condicional de comparativa de precios, y que me filtre por productos
+
+// Un carrito de productos que suma y hace subtotales para pagar es JS
